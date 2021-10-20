b4edef7a5caf844ab638fc12a762f21f3e37b35b
Leandro Perez Guatibonza <leandropg@gmail.com>
2021-10-20 13:23:43 -0500

    Use variables on JSON

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoPageService } from 'src/app/services/info-page.service';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public infoPageService: InfoPageService) { }
 
   ngOnInit(): void {
   }
